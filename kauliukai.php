31f3896058837d955611175bd50d3f5409882fa7
edvinasdau <30698620+edvinasdau@users.noreply.github.com>
2017-09-02 11:45:41 +0300

    kauliukai1

--- kauliukai.php
+++ kauliukai.php
@@ -9,11 +9,11 @@ echo $b . "<br>";
 if ($a == 6 && $b == 6) {
     echo "Bingo";
 } else {
-    echo "ne bingo!<br>";
+    echo "ne bingo!<br><br><br>";
 }
 
 if ($a == 3 || $b == 3) {
-    echo "3 yra !<br><br><br>";
+    #echo "3 yra !<br>";
 }
 
 function go(){
@@ -21,6 +21,30 @@ function go(){
     return $game;
 }
 
-print_r (go());
+function check($aaa){
+    $result ="";
 
+    if ($aaa[0] == 6 && $aaa[1] == 6){
+        $result .= "Bingo!";
+    }
 
+    if ($aaa[0] == $aaa[1]) {
+        $result .= " Vienodi kauliukai";
+    }
+
+    if($aaa[0] < 3 && $aaa[1] < 3){
+        $result .= " Pralosei!";
+    }
+
+    return $result;
+
+}
+
+echo "<table>";
+echo "<tr><td>Zaidimo nr</td><td>Kauliukas A</td><td>Kauliukas B</td><td>Suma</td><td>Rezultatas</td></tr>";
+for ($i=1 ; $i <= 30 ; $i++ ) { 
+    $game = go();
+    echo "<tr><td>" . $i . "</td><td>" . $game[0] . "</td><td>" . $game[1] . "</td><td>" . array_sum($game) . "</td><td>" . check($game) . "</td></tr>";
+}
+
+echo "<table>";
